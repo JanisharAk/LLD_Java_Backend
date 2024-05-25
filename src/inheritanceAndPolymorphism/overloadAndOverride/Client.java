97abe775c3ff674a7c93a4be3290118ff1ed60ce
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-05-25 23:34:36 +0530

    Inheritance and polymorphism(overloadAndOverride)

---
+++ src/inheritanceAndPolymorphism/overloadAndOverride/Client.java
@@ -0,0 +1,24 @@
+package inheritanceAndPolymorphism.overloadAndOverride;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Calculation with calculator");
+        System.out.println(4);
+        Calculator c = new Calculator();
+        c.add(1,2);
+        c.add(1.1,1.2);
+        c.add(1.1,2);
+        c.add(2,3.2);
+
+        Car car = new Car();
+        car.startCar();
+        System.out.println(car.engine);
+        Nano n = new Nano();
+        System.out.println(n.engine);
+        n.startCar();
+
+        Car car1 = new Nano(); //upcasting method
+        car1.startCar();
+        System.out.println(car1.engine);
+    }
+}
