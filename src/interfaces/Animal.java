2bcd5a82195468ff6dd48e4bbb118e7055134fe9
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-05-26 16:02:30 +0530

    Interface

---
+++ src/interfaces/Animal.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Animal {
+    public void eat();
+
+    public void drink();
+
+    public void walk();
+}
---
+++ src/interfaces/Lion.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public class Lion implements Animal {
+    public void eat(){
+        System.out.println("Lion is hunting and eating its prey");
+    }
+
+    public void drink(){
+        System.out.println("Lion is drinking water");
+    }
+
+    public void walk(){
+        System.out.println("Lion is running");
+    }
+}
