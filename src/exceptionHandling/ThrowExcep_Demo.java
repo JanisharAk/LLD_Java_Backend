a0102e4a5c88648566a2ee1cc432a9ff5c4a3737
Janishar Akhtar <janisharakhtar277@gmail.com>
2024-12-26 21:53:06 +0530

    Exception5_ThrowExcep

---
+++ src/exceptionHandling/ThrowExcep_Demo.java
@@ -0,0 +1,24 @@
+package exceptionHandling;
+
+public class ThrowExcep_Demo {
+    static void help()
+    {
+        try {
+            throw new NullPointerException("error_unknown");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Caught inside help().");
+            // rethrowing the exception
+            throw e;
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            help();
+        } catch (NullPointerException e) {
+            System.out.println("Caught in main error name given below:");
+            System.out.println(e);
+        }
+    }
+}
