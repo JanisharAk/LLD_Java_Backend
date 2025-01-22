dd149d5bf0f296311177c879d8430d28d7c673f7
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-22 21:51:32 +0530

    Add code for Part03_Protoype and Registry DP.

---
+++ src/DesignPattern/Part03_prototype/ScalerClient.java
@@ -0,0 +1,25 @@
+package DesignPattern.Part03_prototype;
+
+public class ScalerClient {
+    public static void main(String[] args) {
+        // Template object that imagine has been provided.
+
+        RegistryConfig registryConfig = new RegistryConfig();
+
+        DronaConfig dronaConfig = registryConfig.get("simpleConfig");
+
+        DronaConfig specialConf = registryConfig.get("specialConfig");
+
+        // Main Part
+
+        DronaConfig copy = dronaConfig.clone();
+        copy.account = 91911;
+
+        DronaConfig copy2 = specialConf.clone();
+
+        System.out.println("DEBUG");
+
+
+    }
+
+}
---
+++ src/DesignPattern/Part03_prototype/SpecialConfig.java
@@ -0,0 +1,22 @@
+package DesignPattern.Part03_prototype;
+
+public class SpecialConfig extends DronaConfig {
+    private String specialToken;
+
+    public SpecialConfig(String user, String password, String url, int port, String sessionName, int account, String specialToken) {
+        super(user, password, url, port, sessionName, account);
+        this.specialToken = specialToken;
+    }
+
+
+    public SpecialConfig(SpecialConfig config) {
+        super(config);
+        this.specialToken = config.specialToken;
+    }
+
+    @Override
+    public SpecialConfig clone() {
+        return new SpecialConfig(this);
+    }
+
+}
