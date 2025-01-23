b4a53b55cdf200f745fcafdcbd2e4ec93f5289c1
Janishar Akhtar <janisharakhtar277@gmail.com>
2025-01-23 18:14:14 +0530

    Add code for Part04_factory

---
+++ src/DesignPattern/Part04_factory/simple/Main.java
@@ -0,0 +1,10 @@
+package DesignPattern.Part04_factory.simple;
+
+public class Main {
+    public static void main(String[] args) {
+
+    SimplePizzaFactory simplePizzaFactory = new SimplePizzaFactory();
+    PizzaStore pizzaStore = new PizzaStore(simplePizzaFactory);
+    pizzaStore.orderPizza("Veggie");
+    }
+}
---
+++ src/DesignPattern/Part04_factory/simple/PizzaStore.java
@@ -0,0 +1,24 @@
+package DesignPattern.Part04_factory.simple;
+
+// V1 -> We are only having 1 pizza that we prepare.
+// PizzaStore is responsible for creating different Pizza instances.
+public class PizzaStore {
+    public PizzaStore(SimplePizzaFactory simplePizzaFactory) {
+    }
+
+    Pizza orderPizza(String type) {
+        Pizza pizza;
+        pizza = SimplePizzaFactory.createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+}
+// 1. Dough and put some vegetables - whatever you want
+// 2. Cheese, sauces.
+// 3. Bake the pizza
+// 4. Cut.
+// 5. Box
+// PizzaStore -> orderPizza() -> Pizza.
